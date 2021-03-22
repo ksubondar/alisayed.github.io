bbf57a3c7c3b4820ab1f4507d2a77125ea0cd160
ksubondar <79761397+ksubondar@users.noreply.github.com>
2021-03-22 15:20:27 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 $(function () {
     $(".menuToggle").click(function() {
         $(this).toggleClass("active");
-        $('.header-menu ul').slideToggle(300, function(){
-          if($(this).css('display') === "none"){
-            $(this).removeAttr('style');
+        $(".header-menu ul").slideToggle(300, function(){
+          if($(this).css("display") === "none"){
+            $(this).removeAttr("style"); 
           }
+        });
+        $('.header-menu ul li').on('click', function () {
+          $('.header-menu ul').hide(100, function(){
+            if ($(this).css('display') === 'none') {
+              $(this).removeAttr('style');
+              $(".menuToggle").removeClass("active");
+            }
+          });
         });
       });
     });

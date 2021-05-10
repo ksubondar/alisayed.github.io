d6b7ea3f57bf68745d620272fd32e5805f20f0d4
ksubondar <79761397+ksubondar@users.noreply.github.com>
2021-05-10 15:00:47 +0300

    Update main.js

--- main.js
+++ main.js
@@ -15,4 +15,13 @@ $(function () {
           });
         });
       });
+    $('.nav-link').click(function() {
+        $('html, body').animate({
+    scrollTop: $($(this).attr('href')).offset().top + 'px'
+        }, {
+            duration: 1000,
+            easing: 'swing'
+        });
+    return false;
+    });
     });
